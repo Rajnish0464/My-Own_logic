a039913c0a8e88e5fcd2c719512622ef2bad39f2
Rajnish0464 <105744873+Rajnish0464@users.noreply.github.com>
2022-09-20 05:08:57 +0530

    Update Bubble_sort.cpp

--- Bubble_sort.cpp
+++ Bubble_sort.cpp
@@ -15,7 +15,7 @@ int main() {
         bool swaped=false;
       
         for(int j=0;j<n-1;j++){
-            if(arr[j]<arr[j+1]){
+            if(arr[j]>arr[j+1]){
           swap(arr[j],arr[j+1]);
           swaped=true;
             }
