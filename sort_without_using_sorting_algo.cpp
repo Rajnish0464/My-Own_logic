86c8c3ed0d1a94dbc6b64b518379a85c19a5b74d
Rajnish0464 <105744873+Rajnish0464@users.noreply.github.com>
2022-09-21 03:58:11 +0530

    Create sort_without_using_sorting_algo.cpp

---
+++ sort_without_using_sorting_algo.cpp
@@ -0,0 +1,20 @@
+
+
+
+
+//only main logic of code
+class Solution
+{
+    public:
+    void sort012(int a[], int n)
+    {
+        // code here 
+
+    {
+        sort(a,a+n);
+
+    }
+    }
+    
+};
+
