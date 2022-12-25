e95cf6e568dbdcaa4fadf6fc4f60e8ac8eadb533
Rajnish0464 <105744873+Rajnish0464@users.noreply.github.com>
2022-12-25 05:34:04 +0530

    Create print_E.cpp

---
+++ print_E.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int row,col;
+    for(int row=0;row<7;row++)
+    {
+
+    for(int col=0;col<5;col++)
+    {
+    if((col==0)|| (row==0 || row==3 || row==6))
+    {
+
+            cout<<"*";
+    }
+        else{
+            cout<<" ";
+        }
+        }
+        cout<<endl;
+    }
+
+    return 0;
+}
+
